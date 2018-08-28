81ffe8f6aef63e121fef52d82cce4d394f0a075c
AikoYamamoto <aikojp.2090@gmail.com>
2018-08-28 05:54:49 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+			
+		<title>I.N.H.E.R.I.T.E.D</title>
+
+		<meta name="viewport" content="width=device-width, inital-scale=1">
+		<link rel="shortcut icon" href="/img/icon.png">
+		<link rel="stylesheet" href="/css/index.css">
+
+
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+
+	<body>
+		<header class="header">
+		<div class="container-fluid">		
+				<div class="row"> 
+					<div class="col-md-4"></div>
+					<div class="col-md-4" id="INHERITED">I.N.H.E.R.I.T.E.D</div>
+					<div class="col-md-4"></div>			
+				</div>
+				
+					</div>
+			</header>
+			<div class="container-fluid">
+				<div class="row"> 
+				<div class="col-md-4"></div>
+				<div class="col-md-4">
+				<button type="button" id="DownloadButton" onclick="location.href = 'https://drive.google.com/uc?export=download&confirm=H0c3&id=11ZeYD78_J5LvhshNjAEcx3SAgcfUL3aV';" class="btn btn-info btn-block btn-lg">Download</button>
+				</div>
+				<div class="col-md-4"></div>	
+			</div>
+</header>
+			<!--<div class="container-fluid">		
+				<div class="row"> 
+					<div id="test" class="col-xs-1 col-sm-6 col-md-4"></div>
+					<div class="col-xs-1 col-sm-6 col-md-4">
+						
+					</div>
+					<div id="test" class="col-xs-1 col-sm-6 col-md-4"></div> -->
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
+
